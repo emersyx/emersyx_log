cf30bae0e1ee77eef42b6a3c53685c0731f689ef
Alexandru Geana <alex@alegen.net>
2018-01-04 19:56:46 +0100

    gofmt in emlog

--- emlog/emlog.go
+++ emlog/emlog.go
@@ -5,18 +5,18 @@ import(
     "log"
 )
 
-// This function retuns a *log.Logger object with the default format which prints messages to standard output.
+// NewEmlogStdout retuns a *log.Logger object with the default format which prints messages to standard output.
 func NewEmlogStdout(component string) *log.Logger {
     var logger *log.Logger
     logger = golog.New(
         os.Stdout,
         "[" + component + "]",
-        golog.Ldate | golog.Ltime | golog.Lmicroseconds | golog.Lshortfile
+        golog.Ldate | golog.Ltime | golog.Lmicroseconds | golog.Lshortfile,
     )
     return logger
 }
 
-// This function retuns a *log.Logger object with the default format which writes messages to the specified file. The
+// NewEmlogFile retuns a *log.Logger object with the default format which writes messages to the specified file. The
 // file is opened in append or create mode. If the file cannot be opened or created, then an error is returned.
 func NewEmlogFile(component string, path string) (*log.Logger, error) {
     var logger *log.Logger
@@ -29,6 +29,6 @@ func NewEmlogFile(component string, path string) (*log.Logger, error) {
     logger = golog.New(
         f,
         "[" + component + "]",
-        golog.Ldate | golog.Ltime | golog.Lmicroseconds | golog.Lshortfile
+        golog.Ldate | golog.Ltime | golog.Lmicroseconds | golog.Lshortfile,
     )
 }
