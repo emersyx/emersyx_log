8d29b2e4ea2b8f27ff429891357c24a9c2fb7f59
Alexandru Geana <alex@alegen.net>
2018-01-05 18:43:47 +0100

    multi-level verbosity and file output

--- emlog/emlog.go
+++ emlog/emlog.go
@@ -1,36 +1,65 @@
 package emlog
 
 import (
+	"io"
 	"log"
 	"os"
 )
 
-// NewEmlogStdout retuns a *log.Logger object with the default format which prints messages to standard output.
-func NewEmlogStdout(component string) *log.Logger {
-	var logger *log.Logger
-	logger = log.New(
-		os.Stdout,
-		"["+component+"]",
-		log.Ldate|log.Ltime|log.Lmicroseconds|log.Lshortfile,
-	)
-	return logger
+// Emlog is an implementation of a logger based on the standard log.Logger type. The Emlog implementation offers
+// additional logging functionality
+type Emlog struct {
+	logger *log.Logger
+	level uint
 }
 
-// NewEmlogFile retuns a *log.Logger object with the default format which writes messages to the specified file. The
-// file is opened in append or create mode. If the file cannot be opened or created, then an error is returned.
-func NewEmlogFile(component string, path string) (*log.Logger, error) {
-	var logger *log.Logger
+// Print calls Output to print to the standard logger. Arguments are handled in the manner of fmt.Print.
+func (el Emlog) Print(level uint, v ...interface{}) {
+	if level <= el.level {
+		el.logger.Print(v...)
+	}
+}
 
-	f, err := os.OpenFile("access.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		return nil, err
+// Printf calls Output to print to the standard logger. Arguments are handled in the manner of fmt.Printf.
+func (el Emlog) Printf(level uint, format string, v ...interface{}) {
+	if level <= el.level {
+		el.logger.Printf(format, v...)
+	}
+}
+
+// Println calls Output to print to the standard logger. Arguments are handled in the manner of fmt.Println.
+func (el Emlog) Println(level uint, v ...interface{}) {
+	if level <= el.level {
+		el.logger.Println(v...)
+	}
+}
+
+// NewEmlog retuns an Emlog instance with the default format, which writes messages to standard output if the stdout
+// argument is true and/or the specified file if the path argument is given. If the file cannot be opened or created,
+// then an error is returned. The component argument is prepended to logs for easier filtering, while the level argument
+// controls the verbosity.
+func NewEmlog(stdout bool, path string, component string, level uint) (Emlog, error) {
+	var emlog Emlog
+	var sinks []io.Writer
+
+	if stdout {
+		sinks = append(sinks, os.Stdout)
 	}
 
-	logger = log.New(
-		f,
-		"["+component+"]",
-		log.Ldate|log.Ltime|log.Lmicroseconds|log.Lshortfile,
-	)
+	if len(path) > 0 {
+		f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			return emlog, err
+		}
+		sinks = append(sinks, f)
+	}
 
-	return logger, nil
+	emlog.logger = log.New(
+		io.MultiWriter(sinks...),
+		"["+component+"] ",
+		log.Ldate|log.Ltime,
+	)
+	emlog.level = level
+
+	return emlog, nil
 }
